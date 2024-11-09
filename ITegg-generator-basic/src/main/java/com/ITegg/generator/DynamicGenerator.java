ac53141aba51c792938f9662a071d604f4a0a203
xcscx <1362067461@qq.com>
2024-11-09 14:24:47 +0800

    文件生成方法抽象为静态

--- ITegg-generator-basic/src/main/java/com/ITegg/generator/DynamicGenerator.java
+++ ITegg-generator-basic/src/main/java/com/ITegg/generator/DynamicGenerator.java
@@ -15,14 +15,10 @@ import java.io.Writer;
  */
 public class DynamicGenerator {
     public static void main(String[] args) throws IOException, TemplateException {
-        Configuration configuration = new Configuration(Configuration.VERSION_2_3_31);
 
-        // 标记路径
-        configuration.setDirectoryForTemplateLoading(new File("src/main/resources/templates"));
-        // 文件字符集
-        configuration.setDefaultEncoding("utf-8");
-        // 指定模板
-        Template template = configuration.getTemplate("MainTemplate.java.ftl");
+        String projectPath = System.getProperty("user.dir");
+        String input = projectPath + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
+        String output = projectPath + File.separator + "MainTemplate.java";
 
         // 数据体
         MaintemplateConfig maintemplateConfig = new MaintemplateConfig();
@@ -30,9 +26,33 @@ public class DynamicGenerator {
         maintemplateConfig.setLoop(false);
         maintemplateConfig.setOutputText("求和计算结果");
 
-        // 生成
-        Writer out = new FileWriter("MainTemplate.java");
-        template.process(maintemplateConfig, out);
+        doGenerate(input, output, maintemplateConfig);
+
+
+    }
+
+    /**
+     * 生成文件方法
+     *
+     * @param inputPath 模板文件输入路径
+     * @param outputPath 输出路径
+     * @param model 数据模型
+     */
+    public static void doGenerate(String inputPath, String outputPath, Object model) throws IOException, TemplateException {
+        Configuration configuration = new Configuration(Configuration.VERSION_2_3_31);
+
+        // 标记指定文件的路径
+        File templateDir = new File(inputPath).getParentFile();
+        configuration.setDirectoryForTemplateLoading(templateDir);
+        // 文件字符集
+        configuration.setDefaultEncoding("utf-8");
+        // 指定模板
+        String templateName = new File(inputPath).getName();
+        Template template = configuration.getTemplate(templateName);
+
+        // 生成 outputPath = "MainTemplate.java"
+        Writer out = new FileWriter(outputPath);
+        template.process(model, out);
 
         //关闭流
         out.close();
